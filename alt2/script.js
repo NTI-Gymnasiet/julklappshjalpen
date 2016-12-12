a00c0e3654610599e36a16e21cacffbe87a0f6eb
Kaj Schmidt <kaj.r.schmidt@gmail.com>
2016-12-12 21:26:58 +0100

    moving around

---
+++ alt2/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+
+	// #links smooth scroll
+	$('a').click(function() {
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 1500);
+		return false;
+	});
+	// End of smooth scroll
+
+});
